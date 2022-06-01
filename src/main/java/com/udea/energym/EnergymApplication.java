90d796c9433b081c31da7c8192e3d8d2fe160a92
Duvan Ruiz <duvan.ruiz1@udea.edu.co>
2022-05-31 20:50:29 -0500

    Proyecto inicial

---
+++ src/main/java/com/udea/energym/EnergymApplication.java
@@ -0,0 +1,13 @@
+package com.udea.energym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergymApplication.class, args);
+	}
+
+}
